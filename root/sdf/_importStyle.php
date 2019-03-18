39ccae4bc38bc4421c98470a0fe5c4832a9b61fe
shaklain125 <48572696+shaklain125@users.noreply.github.com>
2019-03-18 06:00:23 +0000

    Cache Fix

--- root/sdf/_importStyle.php
+++ root/sdf/_importStyle.php
@@ -1,3 +1,5 @@
+<html manifest="manifest.appcache">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <link rel="stylesheet" type="text/css" href="css/style.css">
